ca0ddee02d1a17db8153c90442b64538e43ef3d8
Mykola Lebid <realkolos1@gmail.com>
2024-08-17 09:08:39 +0300

    refactor/ remove redundant code

--- api/api.ts
+++ api/api.ts
@@ -1,9 +1,7 @@
+import { getEnvVar } from "@/utils";
+
 import { Api } from "./document";
 
-export const API_URL = process.env.NEXT_PUBLIC_API_URL;
-
-if (!API_URL) {
-  throw new Error("Api Url must be defined as Environment Variable");
-}
+export const API_URL = getEnvVar("NEXT_PUBLIC_API_URL");
 
 export const api = new Api({ baseUrl: API_URL });
--- utils/helpers.ts
+++ utils/helpers.ts
@@ -12,24 +12,3 @@ export function getEnvVar(name: string): string {
   }
   return value;
 }
-
-export async function fetchWithError<T>(
-  url: string,
-  { body, headers, ...options }: RequestInit
-): Promise<T> {
-  const response = await fetch(url, {
-    headers: { "Content-Type": "application/json", ...headers },
-    body,
-    ...options,
-  });
-  if (!response.ok) {
-    const { status } = response;
-    const data = await response.json();
-
-    throw new Error(data.message ?? `Status ${status}: Failed to fetch`);
-  }
-
-  const data = await response.json();
-
-  return data;
-}
